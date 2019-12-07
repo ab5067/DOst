b1d264fab1b0a295b85708251b9df2291cb4a643
Sanchit Monga <sanchitmonga22@gmail.com>
2019-12-07 17:31:09 -0500

    Server and the app is divided into 2 different modules and will run separately, Adding all the protocols functionality to the server now.

--- DostServer/src/main/java/com/example/dostserver/Server.java
+++ DostServer/src/main/java/com/example/dostserver/Server.java
@@ -6,9 +6,8 @@ import java.net.Socket;
 
 public class Server implements Runnable{
     private static ServerSocket server;
-    private int NumberOfUsers;
 
-    public Server(int port){
+    public Server(int port) {
         try {
             this.server=new ServerSocket(port);
         } catch (IOException e) {
@@ -19,10 +18,26 @@ public class Server implements Runnable{
     //TODO: Implement all the functionality to handle all the protocols to and from the client
     @Override
     public void run() {
-        try {
-            Socket user=server.accept();
-        } catch (IOException e) {
-            e.printStackTrace();
+        while (true) {
+            try {
+                Socket user = server.accept();
+                Duplexer duplexer=new Duplexer(user);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+
+    /**
+     * Running the server in the main
+     * @param args
+     */
+    public static void main(String[] args){
+        if(args.length!=3){
+            System.out.println("Usage: java Server <port>");
+        }
+        Server server=new Server(Integer.parseInt(args[2]));
+        server.run();
     }
 }
